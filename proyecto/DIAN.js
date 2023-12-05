9b61f1c7fd3bb90808217c75da913f2e60ed0979
JohanSG18 <90535373+tanner1809@users.noreply.github.com>
2023-12-05 16:18:47 -0500

    Add files via upload
    parcial

--- proyecto/DIAN.js
+++ proyecto/DIAN.js
@@ -8,25 +8,27 @@ const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
 const client = new Client(config);
 
 // Suscribirse al tipo de tarea "SendLetter" y definir la función a ejecutar cuando se recibe una tarea
-client.subscribe("checkRUT", async function({ task, taskService }) {
+client.subscribe("DIANCode", async function({ task, taskService }) {
+
 
     const ID_client = parseInt(task.variables.get("id_name"), 10);  //Traer el cc del cliente 
+    
+    const name_client = task.variables.get("titular_name");  //Traer el cc del cliente 
 
-    //Crear arreglo con la respuesta de la consulta a la BD 
-    const miArreglo = [1006776741, 2222222222, 3333333333, 1121966132];
 
-    // Verificar si la variable está en el arreglo y devolver un booleano
-    const estaEnArreglo = miArreglo.includes(ID_client);
+    const codigo = ID_client+name_client;
+    
     
     // Imprimir el resultado
-    console.log(`La variable ${ID_client} ${estaEnArreglo ? 'está' : 'no está'} en el arreglo.`);
-    console.log({estaEnArreglo});
+    console.log(`El codigo asignado es el siguiente ${codigo}`);
 
-    // Crear un objeto Variables para almacenar variables del proceso
-    const processVariables = new Variables();
+
+    const processVariables = new Variables();s
     
-    processVariables.set("rut_check",estaEnArreglo);
+    processVariables.set("DIANCode",codigo);
+    
     
     // Completar la tarea, proporcionando las variables del proceso
     await taskService.complete(task, processVariables);
+
 });
